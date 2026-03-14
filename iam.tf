d51f4a515b7bd3d51024b80c2eae797bcb1743b6
Kurt Ma. Coll <kurt_ma.coll@live.com>
2026-03-14 14:56:35 +0800

    Create the IAM and the key

---
+++ iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "tsukiyou_identity" {
+  name = "tsukiyou-no-kenzoku"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "bedrock.amazonaws.com"
+        }
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = "${data.aws_caller_identity.current.account_id}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "tsukiyou_vault_access" {
+  name = "TsukiyouVaultAccess"
+  role = aws_iam_role.tsukiyou_identity.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ReadWriteVault"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.moon_estate_bedrock_vault.id}",
+          "arn:aws:s3:::${aws_s3_bucket.moon_estate_bedrock_vault.id}/*"
+        ]
+      },
+      {
+        Sid    = "UseSovereignKey"
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = [aws_kms_key.moon_estate_bedrock_vault_key.arn]
+      }
+    ]
+  })
+}
---
+++ kms.tf
@@ -0,0 +1,43 @@
+resource "aws_kms_key" "moon_estate_bedrock_vault_key" {
+  description             = "Sovereign CMK for Tsukiyou's Vault in the Moon Estate"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Allow Root Account Full Access"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow The SRE Full Management"
+        Effect = "Allow"
+        Principal = {
+          AWS = var.estate_maintainer_arn
+        }
+        Action   = "kms:*" # Full control for the Architect
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow Tsukiyou Cryptographic Usage"
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_iam_role.tsukiyou_identity.arn
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:GenerateDataKey*",
+          "kms:ReEncrypt*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
