aac1b2bcbba191e9b9a1b95d3713cab487c26793
Kurt Ma. Coll <kurt_ma.coll@live.com>
2026-03-21 17:47:56 +0800

    Add gate for SSH

--- gate.tf
+++ gate.tf
@@ -13,3 +13,17 @@ resource "aws_vpc_endpoint" "spirit_gate" {
 
   private_dns_enabled = true
 }
+
+resource "aws_ec2_instance_connect_endpoint" "tsukigumo_sanctum_gate_a" {
+  # The Gate should sit in the Private Subnet (Sanctum) 
+  # to reach the instance directly via the VPC backplane.
+  subnet_id          = aws_subnet.moon_estate_private_a 
+  security_group_ids = [
+    aws_security_group.tsukigumo_ssh_endpoint_sg.id,
+    aws_security_group.tsukigumo_access
+  ]
+
+  tags = {
+    Name = "tsukigumo-seireimon-ssh-gate"
+  }
+}
